1218d74a26d4e4c485cfda85762ed74418d4258e
LuPeBreak <lfbmrj15@gmail.com>
2023-04-28 11:00:48 -0300

    Implementado styled components - thema default - estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-function App() {
+import { ThemeProvider } from 'styled-components'
+import { Header } from './components/Header'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
+export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <Header />
       <h1>Inicio do projeto</h1>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
-
-export default App
