1c94da18ec8af14f612aa4eee319d704c71b2d77
LuPeBreak <lfbmrj15@gmail.com>
2023-05-04 15:41:51 -0300

    Criado arquivo de configuraçao de API

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -10,7 +10,7 @@ export const DefaultLayoutContent = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: -3.5rem auto 0;
+  margin: -4rem auto 0;
   width: 100%;
   max-width: 864px;
   margin-bottom: 2rem;
